a2f3018285b049fb737663508291afebbca995bd
Devin Minnihan <devinminnihan@gmail.com>
2024-08-18 02:32:54 -0400

    add pagination

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -14,3 +14,38 @@ export function formatDate(date: string | number | Date) {
     timeZone: 'UTC',
   });
 }
+
+export function formatPosts(
+  posts: any,
+  {
+    filterOutDrafts = true,
+    fileterOutFuturePosts = true,
+    sortByDate = true,
+    limit = 0,
+  } = {}
+) {
+  const filteredPosts = posts.reduce((acc: any, post: any) => {
+    const { date, draft } = post.frontmatter;
+    if (filterOutDrafts && draft) return acc;
+    if (fileterOutFuturePosts && new Date(date) > new Date()) return acc;
+    acc.push(post);
+    return acc;
+  }, []);
+
+  if (sortByDate) {
+    filteredPosts.sort((a: any, b: any) => {
+      return (
+        new Date(b.frontmatter.date).getTime() -
+        new Date(a.frontmatter.date).getTime()
+      );
+    });
+  } else {
+    filteredPosts.sort(() => Math.random() - 0.5);
+  }
+
+  if (limit) {
+    return filteredPosts.slice(0, limit);
+  }
+
+  return filteredPosts;
+}
